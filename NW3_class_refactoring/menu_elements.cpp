a44a4958995762640fa993ae8cbc65833c024e82
fo1in <fo1inipad@gmail.com>
2025-11-30 13:51:41 +0300

    HW3 in class

---
+++ NW3_class_refactoring/menu_elements.cpp
@@ -0,0 +1,56 @@
+#include "elem_func.h"
+#include "menu_elements.h"
+
+namespace {
+    const folin::MenuItem *const main_children[] = {
+        &folin::STUDY_PROG_LEVEL1
+    };
+}
+
+namespace {
+    const folin::MenuItem *const tech_children[] = {
+        &folin::LEARN_PROG_LANG_LEVEL2,
+        &folin::ALGO_STRUCTURES_LEVEL2,
+        &folin::OTHER_TECH_LEVEL2
+    };
+}
+
+namespace {
+    const folin::MenuItem *const other_tech_children[] = {
+        &folin::DATA_BASE_LEVEL3,
+        &folin::MULTITREADED_PROG_LEVEL3,
+        &folin::NET_TECH_LEVEL3
+    };
+}
+
+const folin::MenuItem folin::MAIN_LEVEL0 {
+    "Главное Меню", MenuFunctions::show_elem_menu, nullptr, main_children, 1
+};
+
+const folin::MenuItem folin::STUDY_PROG_LEVEL1 {
+    "Второй уровень меню", MenuFunctions::show_elem_menu, &MAIN_LEVEL0, tech_children, 3    
+};
+
+const folin::MenuItem folin::LEARN_PROG_LANG_LEVEL2 {
+    "Меню изучения языков программирования", MenuFunctions::show_elem_menu, &STUDY_PROG_LEVEL1, nullptr, 0
+};
+
+const folin::MenuItem folin::ALGO_STRUCTURES_LEVEL2 {
+    "Меню изучения структур и алгоритмов", MenuFunctions::show_elem_menu, &STUDY_PROG_LEVEL1, nullptr, 0
+};
+
+const folin::MenuItem folin::OTHER_TECH_LEVEL2 {
+    "Меню изучения других технологий", MenuFunctions::show_elem_menu, &STUDY_PROG_LEVEL1, other_tech_children, 3
+};
+
+const folin::MenuItem folin::DATA_BASE_LEVEL3 {
+    "Меню изучения баз данных", MenuFunctions::show_elem_menu, &OTHER_TECH_LEVEL2, nullptr, 0
+};
+
+const folin::MenuItem folin::MULTITREADED_PROG_LEVEL3 {
+    "Меню изучения многопоточного программирования", MenuFunctions::show_elem_menu, &OTHER_TECH_LEVEL2, nullptr, 0
+};
+
+const folin::MenuItem folin::NET_TECH_LEVEL3 {
+    "Меню изучения сетевых технологий", MenuFunctions::show_elem_menu, &OTHER_TECH_LEVEL2, nullptr, 0
+};
