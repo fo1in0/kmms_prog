60d6cc710f3ec658682b7ff71ab7f90f6b5970ca
fo1in <fo1inipad@gmail.com>
2025-11-30 12:30:48 +0300

    Refactoring HW3

--- HW3_class/elem_func.cpp
+++ HW3_class/elem_func.cpp
@@ -2,26 +2,25 @@
 #include <iostream>
 
 
-const folin::MenuItem *MenuFunctions::show_global_menu(const folin::MenuItem *current) {
-    if (current->parent == nullptr) {
-        std::cout << "0 - Пойти играть в футбол" << std::endl;
-    } else if (current->parent->parent == nullptr) {
-        std::cout << "0 - Выйти в главное меню" << std::endl;
-    } else {
-        std::cout << "0 - Выйти в предыдущее меню" << std::endl;
-    }
-
+static void print_menu_items(const folin::MenuItem* current) {
     for (int i = 0; i < current->children_count; i++) {
         std::cout << i + 1 << " - " << current->children[i]->title << std::endl;
     }
+}
 
+static int get_user_choice(int children_count) {
     std::cout << "Выберите пункт меню: ";
     int n;
     do {
         std::cin >> n;
-        if (n > current->children_count) std::cout << "Введен неверный номер меню, попробуйте снова!" << std::endl;
-    } while (n > current->children_count);
+        if (n > children_count) {
+            std::cout << "Введен неверный номер меню, попробуйте снова!" << std::endl;
+        }
+    } while (n > children_count);
+    return n;
+}
 
+static const folin::MenuItem* handle_user_choice(const folin::MenuItem* current, int n) {
     if (n == 0) {
         if (current->parent == nullptr) {
             std::cout << "Завершение программы!" << std::endl;
@@ -32,8 +31,23 @@ const folin::MenuItem *MenuFunctions::show_global_menu(const folin::MenuItem *cu
         }
     }
     std::cout << std::endl;
-
     return current->children[n - 1];
+}
+
+const folin::MenuItem *MenuFunctions::show_global_menu(const folin::MenuItem *current) {
+    if (current->parent == nullptr) {
+        std::cout << "0 - Пойти играть в футбол" << std::endl;
+    } else if (current->parent->parent == nullptr) {
+        std::cout << "0 - Выйти в главное меню" << std::endl;
+    } else {
+        std::cout << "0 - Выйти в предыдущее меню" << std::endl;
+    }
+
+    print_menu_items(current);
+
+    int n = get_user_choice(current->children_count);
+
+    return handle_user_choice(current, n);
 }
 
 const folin::MenuItem *MenuFunctions::show_elem_menu(const folin::MenuItem *current) {
