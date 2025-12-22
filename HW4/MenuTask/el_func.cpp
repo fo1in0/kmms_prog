a4cb4c5163caadbf811c6ed09657fa50e5537a34
fo1in <fo1inipad@gmail.com>
2025-12-23 00:46:09 +0300

    Add MenuTask

---
+++ HW4/MenuTask/el_func.cpp
@@ -0,0 +1,50 @@
+#include "el_func.h"
+#include <iostream>
+
+static int get_user_choice(int children_count) {
+    std::cout << "Выберите пункт меню: ";
+    int n;
+    do {
+        std::cin >> n;
+        if (n > children_count) {
+            std::cout << "Введен неверный номер меню, попробуйте снова!" << std::endl;
+        }
+    } while (n > children_count);
+    return n;
+}
+
+static void print_menu_items(const folin::MenuItem* current) {
+    for (int i = 0; i < current->children_count; i++) {
+        std::cout << i + 1 << " - " << current->children[i]->title << std::endl;
+    }
+}
+
+static const folin::MenuItem* handle_user_choice(const folin::MenuItem* current, int n) {
+    if (n == 0) {
+        if (current->parent == nullptr) {
+            std::cout << "Завершение программы!" << std::endl;
+            std::exit(0); 
+        } else {
+            std::cout << "Выйти в главное меню" << std::endl << std::endl;
+            return current->parent;
+        }
+    }
+    std::cout << std::endl;
+    return current->children[n - 1];
+}
+
+const folin::MenuItem *MenuFunctions::show_global_menu(const folin::MenuItem *current) {
+    if (current->parent == nullptr) {
+        std::cout << "0 - Пойти играть в футбол" << std::endl;
+    } else if (current->parent->parent == nullptr) {
+        std::cout << "0 - Выйти в главное меню" << std::endl;
+    } else {
+        std::cout << "0 - Выйти в предыдущее меню" << std::endl;
+    }
+
+    print_menu_items(current);
+
+    int n = get_user_choice(current->children_count);
+
+    return handle_user_choice(current, n);
+}
---
+++ HW4/MenuTask/el_func.h
@@ -0,0 +1,1 @@
+#pragma once#include "menu_item.h"namespace MenuFunctions {    const folin::MenuItem *show_elem_menu(const folin::MenuItem *current);    const folin::MenuItem *show_element_with_link(const folin::MenuItem *current);    const folin::MenuItem *show_global_menu(const folin::MenuItem *current);}
